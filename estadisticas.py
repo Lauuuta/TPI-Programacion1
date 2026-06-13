f49d6f2ba27a26b2c06cd6b7bb184c9bbccc4fac
Lauta <lautadelgadillo27@gmail.com>
2026-06-13 20:40:00 -0300

    Inicializacion funciones (base - filtrar - ordenar) y estadisticas

---
+++ estadisticas.py
@@ -0,0 +1,15 @@
+#Funciones estadisticas paises
+def pais_mayor_poblacion():
+    pass
+
+def pais_menor_poblacion():
+    pass
+
+def promedio_poblacion():
+    pass
+
+def promedio_superficie():
+    pass
+
+def paises_por_continente():
+    pass
---
+++ funciones.py
@@ -0,0 +1,27 @@
+#Funciones base
+def agregar_pais():
+    pass
+
+def actualizar_poblacion_superficie_pais():
+    pass
+
+def buscar_pais():
+    pass
+#Funciones para filtrar paises
+def filtrar_por_continente():
+    pass
+
+def filtrar_por_poblacion():
+    pass
+
+def filtrar_por_superficie():
+    pass
+#Funciones para ordenar paises
+def ordenar_por_nombre():
+    pass
+
+def ordenar_por_poblacion():
+    pass
+
+def ordenar_por_superficie():
+    pass
